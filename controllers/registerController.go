ec047246653a2de01b5c35dea7fe13f4e7484a85
shihualong01128311 <2523629205@qq.com>
2020-09-30 15:06:08 +0800

    second commit

--- controllers/registerController.go
+++ controllers/registerController.go
@@ -3,6 +3,8 @@ package controllers
 import (
 	"BeegoHello0929/db_mysql"
 	"BeegoHello0929/models"
+	"crypto/md5"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"github.com/astaxie/beego"
@@ -22,39 +24,39 @@ func (r *RegisterController)Post() {
 	var user models.User
 	err = json.Unmarshal(dataBytes, &user)
 	if err != nil {
-		r.Ctx.WriteString("数据解析错误，请重试")
-		//result:=models.Result{
-		//Code:0,
-		//Message:"数据解析错误，请重试",
-		//Data:nil,
-	//}
-		//r.Data["json"]=&result
-		//r.ServeJSON()
+		//r.Ctx.WriteString("数据解析错误，请重试")
+		result:=models.Result{
+		Code:0,
+		Message:"数据解析错误，请重试",
+		Data:nil,
+	}
+		r.Data["json"]=&result
+		r.ServeJSON()
 		return
 	}
 	row, err := db_mysql.AddUser(user)
 	if err != nil {
 		fmt.Println(err.Error())
-		r.Ctx.WriteString("注册用户信息失败，请重试")
-		//result:=models.Result{
-			//Code:0,
-			//Message:"注册用户信息失败，请重试",
-			//Data:nil,
-		//}
-		//r.Data["json"]=&result
-		//r.ServeJSON()
+		//r.Ctx.WriteString("注册用户信息失败，请重试")
+		result:=models.Result{
+			Code:0,
+			Message:"注册用户信息失败，请重试",
+			Data:nil,
+		}
+		r.Data["json"]=&result
+		r.ServeJSON()
 		return
 	}
 	fmt.Println(row)
-	//md5Hash:=md5.New()
-	//md5Hash.Write([]byte(user.Password))
-	//user.Password=hex.EncodeToString(md5Hash.Sum(nil))
-	//result:=models.Result{
-	//code:1,
-	//Message:"恭喜，注册用户信息成功",
-	//Data:user,
-//}
-	//r.Data["json"]=&result()
-	//r.ServeJSON()
+	md5Hash:=md5.New()
+	md5Hash.Write([]byte(user.Password))
+	user.Password=hex.EncodeToString(md5Hash.Sum(nil))
+	result:=models.Result{
+	Code:1,
+	Message:"恭喜，注册用户信息成功",
+	Data:user,
+}
+	r.Data["json"]=&result
+	r.ServeJSON()
 	r.Ctx.WriteString("恭喜，注册用户信息成功")
 }
